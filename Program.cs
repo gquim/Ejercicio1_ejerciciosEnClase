9b4b07573b486b6d5ad25c880e3d2cd1189b61ee
Gustavo Quim <gquimb@miumg.edu.gt>
2023-04-15 09:35:22 -0600

    Commit al primer ejercicio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Ingresar numeros del 1 al 12");
+int mes=Convert.ToInt32( Console.ReadLine()) ;
+//int meses;
+if (mes == 1){Console.WriteLine("Enero");}
+if (mes == 2) { Console.WriteLine("Febrero"); }
+if (mes == 3) { Console.WriteLine("Marzo"); }
+if (mes == 4) { Console.WriteLine("Abril"); }
+if (mes == 5) { Console.WriteLine("Mayo"); }
+if (mes == 6) { Console.WriteLine("Junio"); }
+if (mes == 7) { Console.WriteLine("Julio"); }
+if (mes == 8) { Console.WriteLine("Agosto"); }
+if (mes == 9) { Console.WriteLine("Septiembre"); }
+if (mes == 10) { Console.WriteLine("Octubre"); }
+if (mes == 11) { Console.WriteLine("Noviembre"); }
+if (mes == 12) { Console.WriteLine("Diciembre"); }
+else{Console.WriteLine("Numero no reconocido para mostrar un mes");}
