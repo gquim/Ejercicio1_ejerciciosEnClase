28fb07de46ef5c56da55aa30c4589c32ab53bc53
Gustavo Quim <gquimb@miumg.edu.gt>
2023-04-15 12:27:47 -0600

    Mejoramiento del codigo

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+//
 
 Console.WriteLine("Ingresar numeros del 1 al 12");
 int mes=Convert.ToInt32( Console.ReadLine()) ;
-//int meses;
-if (mes == 1){Console.WriteLine("Enero");}
-if (mes == 2) { Console.WriteLine("Febrero"); }
-if (mes == 3) { Console.WriteLine("Marzo"); }
-if (mes == 4) { Console.WriteLine("Abril"); }
-if (mes == 5) { Console.WriteLine("Mayo"); }
-if (mes == 6) { Console.WriteLine("Junio"); }
-if (mes == 7) { Console.WriteLine("Julio"); }
-if (mes == 8) { Console.WriteLine("Agosto"); }
-if (mes == 9) { Console.WriteLine("Septiembre"); }
-if (mes == 10) { Console.WriteLine("Octubre"); }
-if (mes == 11) { Console.WriteLine("Noviembre"); }
-if (mes == 12) { Console.WriteLine("Diciembre"); }
-else{Console.WriteLine("Numero no reconocido para mostrar un mes");}
+
+string nombreMes;
+
+switch(mes)
+{
+    case 1: nombreMes="enero"; break;
+    case 2: nombreMes = "febrero"; break;
+    case 3: nombreMes = "marzo"; break;
+    case 4: nombreMes = "abril"; break;
+    case 5: nombreMes = "mayo"; break;
+    case 6: nombreMes = "junio"; break;
+    case 7: nombreMes = "julio"; break;
+    case 8: nombreMes = "agosto"; break;
+    case 9: nombreMes = "septiembre"; break;
+    case 10: nombreMes = "octubre"; break;
+    case 11: nombreMes = "noviembre"; break;
+    case 12: nombreMes = "diciembre"; break;
+     default: Console.WriteLine("no hay mes con esa numeracion"); return;
+}
+Console.WriteLine("El mes correspondiente es: " + nombreMes);
